f7c157db486e78fbda1c94d8205ea425873e5b8e
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-03-07 12:29:04 +0500

    Update config.exs

--- config/config.exs
+++ config/config.exs
@@ -10,8 +10,7 @@ config :webrtc_signaling, WebrtcSignalingWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "iDiJIaouYkqumqZnzLRxBZEUMjTyRDz+oqgEpvgSb9+BNazCFb5Id7guAFkHJFCt",
   render_errors: [view: WebrtcSignalingWeb.ErrorView, accepts: ~w(json)],
-  pubsub: [name: WebrtcSignaling.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: WebrtcSignaling.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -20,4 +19,4 @@ config :logger, :console,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
