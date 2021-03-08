f51051c6ec1cef43b9bf2ab3c85c8900393e0cd7
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-03-09 01:23:29 +0500

    Handling webrtcoffer

--- lib/webrtc_signaling_web/channels/room_channel.ex
+++ lib/webrtc_signaling_web/channels/room_channel.ex
@@ -4,4 +4,11 @@ defmodule WebrtcSignalingWeb.RoomChannel do
   def join("channel:signal", %{}, socket) do
     {:ok, %{}, socket}
   end
+
+
+  def handle_in("channel:sendOffer", %{"sender" => sender, "offer" => offer}, socket) do
+    broadcast!(socket, "channel:onOffer", %{"sender" => sender, "offer" => offer})
+    {:noreply, socket}
+  end
+
 end
