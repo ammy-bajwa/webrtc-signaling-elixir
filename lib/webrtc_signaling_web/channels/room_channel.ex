0bd94c3401770a80d025a14776072fb2f6617cc4
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-03-09 01:24:28 +0500

    Adding webrtc signaling service

--- lib/webrtc_signaling_web/channels/room_channel.ex
+++ lib/webrtc_signaling_web/channels/room_channel.ex
@@ -5,9 +5,18 @@ defmodule WebrtcSignalingWeb.RoomChannel do
     {:ok, %{}, socket}
   end
 
-
   def handle_in("channel:sendOffer", %{"sender" => sender, "offer" => offer}, socket) do
     broadcast!(socket, "channel:onOffer", %{"sender" => sender, "offer" => offer})
+    {:noreply, socket}
+  end
+
+  def handle_in("channel:sendAnswer", %{"sender" => sender, "answer" => answer}, socket) do
+    broadcast!(socket, "channel:onAnswer", %{"sender" => sender, "answer" => answer})
+    {:noreply, socket}
+  end
+
+  def handle_in("channel:sendIce", %{"sender" => sender, "candidate" => candidate}, socket) do
+    broadcast!(socket, "channel:onCandidate", %{"sender" => sender, "candidate" => candidate})
     {:noreply, socket}
   end
 
