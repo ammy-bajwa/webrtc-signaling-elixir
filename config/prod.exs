6ade3a4d4e935de3f1f477d6b2d612bad1bafece
AbdulRehman197 <38237505+AbdulRehman197@users.noreply.github.com>
2021-03-22 14:40:17 +0500

    Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :webrtc_signaling, WebrtcSignalingWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "https://lan-web-rtc.herokuapp.com/", port: 443],
+  url: [scheme: "https", host: "https://abc-webrtc.herokuapp.com/", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   check_origin: ["https://abc-webrtc.herokuapp.com"]
   # cache_static_manifest: "priv/static/cache_manifest.json"
