4dbdff2afc4570706a6defb6c56f67b6ab3982dd
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-03-27 10:57:07 +0500

    Adding fileName

--- lib/webrtc_signaling_web/channels/room_channel.ex
+++ lib/webrtc_signaling_web/channels/room_channel.ex
@@ -10,13 +10,28 @@ defmodule WebrtcSignalingWeb.RoomChannel do
     {:noreply, socket}
   end
 
+  def handle_in("channel:sendOfferFilePC", %{"sender" => sender, "offer" => offer, "fileName" => file_name}, socket) do
+    broadcast!(socket, "channel:onOfferFilePC__#{file_name}", %{"sender" => sender, "offer" => offer, "fileName" => file_name})
+    {:noreply, socket}
+  end
+
   def handle_in("channel:sendAnswer", %{"sender" => sender, "answer" => answer}, socket) do
     broadcast!(socket, "channel:onAnswer", %{"sender" => sender, "answer" => answer})
     {:noreply, socket}
   end
 
+  def handle_in("channel:sendAnswerFilePC", %{"sender" => sender, "answer" => answer, "fileName" => file_name}, socket) do
+    broadcast!(socket, "channel:onAnswerFilePC__#{file_name}", %{"sender" => sender, "answer" => answer, "fileName" => file_name})
+    {:noreply, socket}
+  end
+
   def handle_in("channel:sendIce", %{"sender" => sender, "candidate" => candidate}, socket) do
     broadcast!(socket, "channel:onCandidate", %{"sender" => sender, "candidate" => candidate})
+    {:noreply, socket}
+  end
+
+  def handle_in("channel:sendIceFilePC", %{"sender" => sender, "candidate" => candidate, "fileName" => file_name}, socket) do
+    broadcast!(socket, "channel:onCandidateFilePC__#{file_name}", %{"sender" => sender, "candidate" => candidate})
     {:noreply, socket}
   end
 
